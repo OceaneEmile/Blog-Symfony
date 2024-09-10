a2eaab1c2040f1cb292ae53c596551a93bdf24b0
oceaneEmile <oceane.emile@oclock.school>
2024-09-10 09:17:12 +0200

    API/ABOUT route

---
+++ Blog/src/Controller/AboutController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+class AboutController extends AbstractController
+{
+    // method GET for the route /about
+    #[Route('api/about', name: 'app_about', methods: ['GET'])]
+    public function index(): JsonResponse
+    {
+        // data to return in the response body to modify later
+        $data = [
+            'controller_name' => 'AboutController',
+            'message' => 'Bienvenue sur mon blog de voyage !',
+            'status' => 'success'
+        ];
+
+        return new JsonResponse($data);
+    }
+}
